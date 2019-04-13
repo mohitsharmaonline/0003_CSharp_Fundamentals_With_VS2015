07c522d13d14c0d24d876fa03fc2d4bd44d640a3
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-13 20:25:04 +0530

    Theory and code upto 006 The C# Language.

---
+++ 0002_An_Intro_To_CSharp_Dotnet/Ex_0001_HelloCSharp/Ex_0001_HelloCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_0001_HelloCSharp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            if(DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+            {
+                Console.WriteLine("Another case of the Saturdays!");
+            }
+        }
+    }
+}
