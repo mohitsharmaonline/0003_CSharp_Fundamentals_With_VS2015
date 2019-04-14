78594c096809937c6c12d3766b396029dc17987d
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-14 14:13:43 +0530

    Initial version of Hello

---
+++ 0002_An_Intro_To_CSharp_Dotnet/Ex_0002_Hello/Ex_0002_Hello/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Ex_0002_Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
