c420116fdd205b85e294b951ec6e3e3f48b8956d
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-19 13:48:37 +0530

    Program Part 2

--- 0003_Classes_and_Objects/Grades/Grades/Program.cs
+++ 0003_Classes_and_Objects/Grades/Grades/Program.cs
@@ -13,8 +13,8 @@ namespace Grades
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
-            book = new GradeBook();
-            book.AddGrade(75);
+            GradeBook  book2 = new GradeBook();
+            book2.AddGrade(75);
         }
     }
 }
