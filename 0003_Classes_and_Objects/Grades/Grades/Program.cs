66900fb3e4e8b688eb87a0eca86df7deef23d2cb
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-19 13:49:37 +0530

    Code Version 3

--- 0003_Classes_and_Objects/Grades/Grades/Program.cs
+++ 0003_Classes_and_Objects/Grades/Grades/Program.cs
@@ -13,7 +13,7 @@ namespace Grades
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
-            GradeBook  book2 = new GradeBook();
+            GradeBook  book2 = book;
             book2.AddGrade(75);
         }
     }
