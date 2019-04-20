e1b1444a4cf34e17a9f02c8cdca607a152c2882b
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-20 18:40:31 +0530

    New test with demo of float equality failure in Unit Tests.

--- 0003_Classes_and_Objects/Grades/Grades.Tests/GradeBookTests.cs
+++ 0003_Classes_and_Objects/Grades/Grades.Tests/GradeBookTests.cs
@@ -32,5 +32,17 @@ namespace Grades.Tests
             GradeStatistics result = book.ComputeStatistics();
             Assert.AreEqual(10, result.LowestGrade);
         }
+
+        [TestMethod]
+        public void ComputeAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.AverageGrade);
+        }
     }
 }
