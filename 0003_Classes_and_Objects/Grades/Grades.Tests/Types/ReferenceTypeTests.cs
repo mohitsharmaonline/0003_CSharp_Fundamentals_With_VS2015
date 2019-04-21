2aac4b1bb906400536f001c9c82e94adf3ca3f3a
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-21 17:30:18 +0530

    ReferenceTypesPassByValue Test.

--- 0003_Classes_and_Objects/Grades/Grades.Tests/Types/ReferenceTypeTests.cs
+++ 0003_Classes_and_Objects/Grades/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -11,6 +11,21 @@ namespace Grades.Tests.Types
     public class TypeTests
     {
         [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A GradeBook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A GradeBook";
+        }
+
+        [TestMethod]
         public void StringComparisons()
         {
             string name1 = "Scott";
