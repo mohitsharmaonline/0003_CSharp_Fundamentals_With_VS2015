9fd1c8a33f31459661800cccb94703f4c04c7030
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-22 16:17:41 +0530

    How client code can misuse auto implemented properties.

--- 0003_Classes_and_Objects/Grades/Grades/Program.cs
+++ 0003_Classes_and_Objects/Grades/Grades/Program.cs
@@ -12,6 +12,7 @@ namespace Grades
         {
             GradeBook book = new GradeBook();
             book.Name = "Scott's Grade Book";
+            book.Name = null;
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
