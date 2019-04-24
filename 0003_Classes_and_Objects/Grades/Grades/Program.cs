d15b894b8fb76d555859345444d750788936ee1d
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-25 00:37:26 +0530

    code to display multi subscription in events too.

--- 0003_Classes_and_Objects/Grades/Grades/Program.cs
+++ 0003_Classes_and_Objects/Grades/Grades/Program.cs
@@ -13,7 +13,8 @@ namespace Grades
             GradeBook book = new GradeBook();
             book.NameChanged += new NameChangedDelegate(OnNameChanged);
             book.NameChanged += new NameChangedDelegate(OnNameChanged2);
-            
+            book.NameChanged += new NameChangedDelegate(OnNameChanged2);
+
             book.Name = "Scott's Grade Book";
             book.Name = "Grade Book";
             book.Name = null;
