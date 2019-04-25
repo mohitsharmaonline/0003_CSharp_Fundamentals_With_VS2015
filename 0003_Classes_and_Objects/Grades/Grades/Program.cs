a66cf80929142ed1bcf86ebe58ee24d3354b2f1a
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-25 21:34:14 +0530

    code cleanup to make space for Branching changes.

--- 0003_Classes_and_Objects/Grades/Grades/Program.cs
+++ 0003_Classes_and_Objects/Grades/Grades/Program.cs
@@ -11,11 +11,7 @@ namespace Grades
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
-            book.NameChanged += OnNameChanged;
-
-            book.Name = "Scott's Grade Book";
-            book.Name = "Grade Book";
-            book.Name = null;
+            
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
@@ -23,21 +19,10 @@ namespace Grades
             GradeStatistics statistics = book.ComputeStatistics();
             Console.WriteLine(book.Name);
             WriteResult("Average", statistics.AverageGrade);
-            WriteResult("Highest", (int)statistics.HighestGrade);
+            WriteResult("Highest", statistics.HighestGrade);
             WriteResult("Lowest", statistics.LowestGrade);
         }
-
-        static void OnNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine($"Gradebook is changing name from {args.ExistingName} to {args.NewName}");
-        }
-
         
-        static void WriteResult(string description, int result)
-        {
-            Console.WriteLine(description + ": " + result);
-        }
-
         static void WriteResult(string description, float result)
         {
             Console.WriteLine($"{description}: {result:F2}");
