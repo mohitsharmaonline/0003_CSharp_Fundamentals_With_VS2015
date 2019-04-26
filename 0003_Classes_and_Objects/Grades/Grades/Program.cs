741d6832175aeaf8fb8de2caa11b5e373df71506
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-04-26 22:05:08 +0530

    Exception Handling code and Theory.

--- 0003_Classes_and_Objects/Grades/Grades/Program.cs
+++ 0003_Classes_and_Objects/Grades/Grades/Program.cs
@@ -8,7 +8,15 @@ namespace Grades
         {
             GradeBook book = new GradeBook();
 
-            book.Name = null;
+            try
+            {
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
             
             book.AddGrade(91);
             book.AddGrade(89.5f);
